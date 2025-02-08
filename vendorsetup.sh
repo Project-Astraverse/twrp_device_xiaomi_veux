b71979f6355708f935f818950d158eabd5e363b2
cachiusa <nhat.dogpro@outlook.com>
2025-02-08 16:27:21 +0700

    veux: Remove misc build flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,15 +10,5 @@ if [ -z "$BASH_SOURCE" ]; then
   echo "warn: This command should be run with /bin/bash shell"
 fi
 
-set -a
-
-# Build hack
-ALLOW_MISSING_DEPENDENCIES=true
-
-# Force default locale
-LC_ALL="C"
-
-set +a
-
 # Call other setup scripts
 bash "${VENDORSETUP_PATH}/twrp.dependencies.sh"
